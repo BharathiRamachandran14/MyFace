ee136e85f9d6f95812e1e6a88e8b4eb1d649e674
Bharathi Ramachandran <Bharathi.Ramachandran@softwire.com>
2022-08-26 10:03:43 +0100

    updated by cloning the project we worked in pairs

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Add your JavaScript here.
+  const button=document.getElementById("colourChange");
+  button.addEventListener("click",function(){
+    const postimages=document.getElementsByClassName("postImage");
+    for(let i=0;i<postimages.length;i++){
+      postimages[i].style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    }
+  })
 });
