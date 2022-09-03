9881dedeee665f47962c580241633a8d241760e8
Bharathi Ramachandran <Bharathi.Ramachandran@softwire.com>
2022-09-03 09:23:06 +0100

    updates the menu

--- public/js/app.js
+++ public/js/app.js
@@ -7,4 +7,17 @@ document.addEventListener("DOMContentLoaded", function () {
       postimages[i].style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
     }
   })
+ const btn=document.getElementById("btnmenu");
+ btn.addEventListener("click", function(){
+   const dropdown=document.getElementById("dropdown");
+   
+   dropdown.className="dropdown dropdown-open";
+ })
+ // You could separate the display:none property from the dropdown class, and put it in its own dropdown-closed class
+ // Then every time the user opens or closes the menu you can switch the class name
+ // So it will always have the dropdown class applied to give the text etc styles
+ // But we'll switch just the dropdown-open and dropdown-closed classes on click so that we change the display property
+
+  
+
 });
